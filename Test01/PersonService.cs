f2fc1f93063513993e49e1358f53ce254b60ecd5
animeonelove2190 <daniil.davletshin1@gmail.com>
2023-09-04 04:32:18 +0500

    Sort List

--- Test01/PersonService.cs
+++ Test01/PersonService.cs
@@ -6,23 +6,42 @@ namespace Test01
 {
     class PersonService
     {
-        public PersonFullInfo SetPersonFullInfo(PersonInfo persoInfo, PersonParams personParams)
+        readonly TechnicalService techService = new TechnicalService();
+        public List<Person> SortPersonByName(List<Person> list)
         {
-            PersonFullInfo personFullInfoSer = new PersonFullInfo();
-            personFullInfoSer.name = persoInfo.name;
-            personFullInfoSer.floor = persoInfo.floor;
-            personFullInfoSer.age = personParams.age;
-            personFullInfoSer.weight = personParams.weight;
-            personFullInfoSer.height = personParams.height;
-            return personFullInfoSer;
-        }
-        public void WritePerson(PersonFullInfo personFullInfo)
-        {
-            Console.WriteLine(personFullInfo.name);
-            Console.WriteLine(personFullInfo.floor);
-            Console.WriteLine(personFullInfo.age);
-            Console.WriteLine(personFullInfo.weight);
-            Console.WriteLine(personFullInfo.height);
+            if (list == null || list.Count == 0)
+            {
+                Console.WriteLine("Сработала защита в GetPersonWithMinAge");
+                return null;
+            }
+            char[] charArray = new char[list.Count];
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                charArray[i] = list[i].Name[0];
+            }
+            techService.BubbleSort(charArray);
+            List<Person> nameSortList = new List<Person>();
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                for (int j = 0; j < list.Count; j++)
+                {
+                    if (list[j].Name[0] == charArray[i])
+                    {
+                        nameSortList.Add(list[j]);
+                    }
+                }
+            }
+            for (int i = 0; i < nameSortList.Count; i++)
+            {
+                for (int j = i + 1; j < nameSortList.Count; j++)
+                {
+                    if (nameSortList[i] == nameSortList[j])
+                    {
+                        nameSortList.RemoveAt(j);
+                    }
+                }
+            }
+            return nameSortList;
         }
     }
 }
--- Test01/Program.cs
+++ Test01/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Test01
 {
@@ -6,26 +7,41 @@ namespace Test01
     {
         static void Main(string[] args)
         {
-            PersonInfo misatoInfo = new PersonInfo();
-            misatoInfo.name = "Misato Katsuragi";
-            misatoInfo.floor = "Female";
-            PersonInfo asunaInfo = new PersonInfo();
-            asunaInfo.name = "Yuuki Asuna";
-            asunaInfo.floor = "Female";
-            PersonParams misatoParams = new PersonParams();
-            misatoParams.age = 29;
-            misatoParams.weight = 47;
-            misatoParams.height = 163;
-            PersonParams asunaParams = new PersonParams();
-            asunaParams.age = 19;
-            asunaParams.weight = 54;
-            asunaParams.height = 168;
-            PersonService personServiceSetInfo = new PersonService();
-            PersonFullInfo personFullInfoMisato = personServiceSetInfo.SetPersonFullInfo(misatoInfo, misatoParams);
-            PersonService writeService = new PersonService();
-            writeService.WritePerson(personFullInfoMisato);
-            PersonFullInfo personFullInfoAsuna = personServiceSetInfo.SetPersonFullInfo(asunaInfo, asunaParams);
-            writeService.WritePerson(personFullInfoAsuna);
+            TechnicalService techService = new TechnicalService();
+            PersonService personService = new PersonService();
+            Person personOne = new Person();
+            personOne.Name = "Misato";
+            personOne.Age = 29;
+            personOne.Weight = 47;
+            personOne.Height = 163;
+            Person personTwo = new Person();
+            personTwo.Name = "Asuna";
+            personTwo.Age = 19;
+            personTwo.Weight = 54;
+            personTwo.Height = 168;
+            Person personThree = new Person();
+            personThree.Name = "Rafutaria";
+            personThree.Age = 10;
+            personThree.Weight = 56;
+            personThree.Height = 170;
+            Person personFour = new Person();
+            personFour.Name = "Hanji";
+            personFour.Age = 31;
+            personFour.Weight = 60;
+            personFour.Height = 170;
+            Person personFive = new Person();
+            personFive.Name = "Violet";
+            personFive.Age = 18;
+            personFive.Weight = 42;
+            personFive.Height = 161;
+            List<Person> waifuList = new List<Person>();
+            waifuList.Add(personOne);
+            waifuList.Add(personTwo);
+            waifuList.Add(personThree);
+            waifuList.Add(personFour);
+            waifuList.Add(personFive);
+            List<Person> nameSortList = personService.SortPersonByName(waifuList);
+            techService.WriteList(nameSortList);
         }
     }
 }
