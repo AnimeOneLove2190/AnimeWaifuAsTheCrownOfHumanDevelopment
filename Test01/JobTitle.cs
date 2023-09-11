153be9f1dce25c1f101d7c0bb7081d6f85627f7e
animeonelove2190 <daniil.davletshin1@gmail.com>
2023-09-12 00:44:17 +0500

    h

---
+++ Test01/JobTitle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test01
+{
+    enum JobTitle
+    {
+        Mafia,
+        RoyalArmySpecialOfficer,
+        AutoRecordingDoll,
+        CommanderOfTheParadiseIslandSurveyCorps
+    }
+}
