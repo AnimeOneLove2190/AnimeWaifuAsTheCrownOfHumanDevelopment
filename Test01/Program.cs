fe7878e257c00a58280fd6c53b03b5c6b14a2002
animeonelove2190 <daniil.davletshin1@gmail.com>
2023-11-08 04:21:21 +0500

    Промежуточная конвертация

--- Test01/Program.cs
+++ Test01/Program.cs
@@ -13,123 +13,25 @@ namespace Test01
         static void Main(string[] args)
         {
             var tech = new TechService();
-            //565
-            var misato = new Student
+            var laputa = new UniversalClass
             {
-                Name = "Misato",
-                Age = 29,
-                CountOfFriends = 4,
-                Spezialisation = Spezialitations.EvangelionSquadTacticalOperationsCommander,
-            };
-            var asuna = new Student
-            {
-                Name = "Asuna",
-                Age = 19,
-                CountOfFriends = 14,
-                Spezialisation = Spezialitations.SubleaderOfTheBloodKnightsGuild,
-            }; 
-            var violet = new Student
-            {
-                Name = "Violet",
-                Age = 18,
-                CountOfFriends = 6,
-                Spezialisation = Spezialitations.AutoRecordingDoll,
-            };
-            var senko = new Student
-            {
-                Name = "Senko",
-                Age = 804,
-                CountOfFriends = 4,
-                Spezialisation = Spezialitations.GoddessOfFertility,
-            };
-            var hanji = new Student
-            {
-                Name = "Hanji",
-                Age = 31,
-                CountOfFriends = 2,
-                Spezialisation = Spezialitations.CommanderOfTheParadiseIslandSurveyCorps,
-            };
-            var jotaro = new Student
-            {
-                Name = "Jotaro",
-                Age = 53,
-                CountOfFriends = 5,
-                Spezialisation = Spezialitations.StandUserStandoPawarZaWarudo,
-            };
-            var studentList = new List<Student>
-            {
-                misato,
-                asuna,
-                violet,
-                senko,
-                hanji,
-                jotaro
-            };
-            var firstStudentWithName = studentList.FirstOrDefault(x => x.Name == "Violet");
-            if (firstStudentWithName == null)
-            {
-                throw new Exception("Студент с выбранным именем не найден");
-            }
-            else
-            {
-                Console.WriteLine(firstStudentWithName.Name);
-                Console.WriteLine(firstStudentWithName.Age);
-                Console.WriteLine(firstStudentWithName.CountOfFriends);
-            }
-            //567
-            var firstStudentWithAge = studentList.FirstOrDefault(x => x.Age > 20);
-            if (firstStudentWithAge == null)
-            {
-                throw new Exception("Студент с искомым возрастом не найден");
-            }
-            else
-            {
-                Console.WriteLine(firstStudentWithAge.Name);
-                Console.WriteLine(firstStudentWithAge.Age);
-                Console.WriteLine(firstStudentWithAge.CountOfFriends);
-            }
-            //568
-            var studentsWithAgeList = studentList.Where(x => x.Age == 19).ToList();
-            if (studentsWithAgeList == null || studentsWithAgeList.Count == 0)
-            {
-                throw new Exception("Студенты с искомым возрастом не найдены");
-            }
-            else
-            {
-                foreach(var student in studentsWithAgeList)
+                Id = 1,
+                Name = "Tenkuu no Shiro Laputa",
+                Description = "Мяв. Альтернативная реальность, соответствующая началу XX века. В руках девочки по имени Сита находится Летающий Камень. За ним охотятся агенты правительства и пираты, потому что Камень представляет огромную ценность. Пытаясь скрыться от преследователей, Сита встречает Падзу, своего ровесника, работающего в шахтерском городке. Вместе дети выясняют, что Камень — ключ к таинственному летающему острову Лапута.",
+                Duration = 125,
+                GenreIds = new List<int>
                 {
-                    Console.WriteLine(student.Name);
-                    Console.WriteLine(student.Age);
-                    Console.WriteLine(student.CountOfFriends);
+                    1,
+                    2,
+                    3,
+                    4,
+                    5,
                 }
-            }
-            //570
-            var studentsSpecList = studentList.Where(x => x.Spezialisation == Spezialitations.AutoRecordingDoll).ToList();
-            if (studentsSpecList == null || studentsSpecList.Count == 0)
+            };
+            string movieJson = JsonConvert.SerializeObject(laputa, Formatting.Indented);
+            using (StreamWriter somePlaylist = File.CreateText("Some Movie.json"))
             {
-                throw new Exception("Студенты с искомой специализацией не найдены");
-            }
-            else
-            {
-                foreach (var student in studentsSpecList)
-                {
-                    Console.WriteLine(student.Name);
-                    Console.WriteLine(student.Age);
-                    Console.WriteLine(student.CountOfFriends);
-                }
-            }
-            //571
-            var studentsNameList = studentList.Select(x => x.Name).ToList();
-            if (studentsNameList == null || studentsNameList.Count == 0)
-            {
-                throw new Exception("Студенты с искомым возрастом не найдены");
-            }
-            else
-            {
-                foreach (var name in studentsNameList)
-                {
-                    Console.WriteLine(name);
-                }
+                somePlaylist.Write(movieJson);
             }
         }
     }
---
+++ Test01/UniversalClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test01
+{
+    public class UniversalClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Duration { get; set; }
+        public List<int> GenreIds { get; set; }
+    }
+}
